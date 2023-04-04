48101b619f5d08679c0f6e78711cd28b09f8bab7
Jennif6r <85253972+Jennif6r@users.noreply.github.com>
2023-04-04 22:01:40 +0200

    initiate Client

---
+++ Client/src/main/java/services/Registrator.java
@@ -0,0 +1,14 @@
+package services;
+
+import jakarta.jws.WebMethod;
+import jakarta.jws.WebService;
+import jakarta.jws.soap.SOAPBinding;
+
+@WebService
+@SOAPBinding(style = SOAPBinding.Style.RPC)
+public interface Registrator {
+	
+	@WebMethod
+	String registerClient();
+
+}
---
+++ Client/src/main/java/services/impl/RegistratorClient.java
@@ -0,0 +1,22 @@
+package services.impl;
+
+import java.net.URL;
+
+import javax.xml.namespace.QName;
+
+import jakarta.xml.ws.Service;
+import services.Registrator;
+
+public class RegistratorClient {
+	public static void main(String[] args) throws Exception {
+		URL serviceUrl = new URL("http://localhost:9000/register?wsdl");
+		
+		QName serviceQName = new QName("http://impl.services.java.main/", "RegistratorImplService");
+		Service service = Service.create(serviceUrl ,serviceQName);
+		
+		QName registerQName = new QName("http://impl.services.java.main/", "RegistratorImplPort");
+		Registrator registrator = service.getPort(registerQName, Registrator.class);
+		
+		System.out.println(registrator.registerClient());
+	}
+}
