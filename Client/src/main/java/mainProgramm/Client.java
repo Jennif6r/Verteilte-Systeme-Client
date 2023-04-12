5ed44a2ebd1e1351577019093b4eb4921a443890
0Raspbinary1 <rgremsperger@googlemail.com>
2023-04-13 00:02:45 +0200

    connected gui with client

--- Client/src/main/java/mainProgramm/Client.java
+++ Client/src/main/java/mainProgramm/Client.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import gui.OrderGUI;
 import models.Order;
 import models.OrderList;
 import models.Product;
@@ -18,33 +19,40 @@ import services.impl.StartOrderClient;
 
 public class Client {
 	private String id;
+	public String orderId;
 
 	public static void main(String[] args) {
+		
+//			
 		Client client = new Client();
-		try {
-//			client.id = RegistratorClient.register();
-//			System.out.println(client.id);
-//			// request for aktiv order
-			RequestAktivOrderClient request = new RequestAktivOrderClient();
-//			client.startOrder();
-			List<OrderList> activOrders = new ArrayList<OrderList>(Arrays.asList(request.checkForAktivOrder()));
-			int length = activOrders.size();
-			if( length > 0 ) {
-				System.out.println(activOrders.toString());
-			}
-//			client.addOrderItem();
-//			client.getNumberOfOrders();
-//			while (!request.checkForAktivOrder()) {
-//				// wait and do nothing
+//		
+		OrderGUI orderGUI = new OrderGUI(client);
+//		
+//		
+//		try {
+////			client.id = RegistratorClient.register();
+////			System.out.println(client.id);
+////			// request for aktiv order
+//			RequestAktivOrderClient request = new RequestAktivOrderClient();
+////			client.startOrder();
+//			List<OrderList> activOrders = new ArrayList<OrderList>(Arrays.asList(request.checkForAktivOrder()));
+//			int length = activOrders.size();
+//			if( length > 0 ) {
+//				System.out.println(activOrders.toString());
 //			}
-			// here code to show frame to order
-//			client.getMergedOrder();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+////			client.addOrderItem();
+////			client.getNumberOfOrders();
+////			while (!request.checkForAktivOrder()) {
+////				// wait and do nothing
+////			}
+//			// here code to show frame to order
+////			client.getMergedOrder();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
 	}
 	
-	private void startOrder() {
+	public String startOrder() {
 		StartOrderClient start = new StartOrderClient();
 		String orderId = "";
 		try {
@@ -52,40 +60,40 @@ public class Client {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		System.out.println("id: "+ orderId);
+		this.orderId = orderId;
+//		System.out.println("id: "+ orderId);
+		return orderId;
 	}
 	
-	public void addOrderItem() {
+	public void addOrderItem(Order order, String orderId) {
 		AddOrderItemClient add = new AddOrderItemClient();
-		ArrayList<Product> products = getProducts();
 		try {
-			Order order = new Order();
-			order.addProduct(products);
-			add.addOrderItem("1680778200745", JsonParser.parseOrder(order));
+			
+			add.addOrderItem(orderId, JsonParser.parseOrder(order));
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	private ArrayList<Product> getProducts() {
+	public ArrayList<Product> getProducts() {
 		ArrayList<Product> products = new ArrayList<Product>();
 		products.add(new Product("Magertia", 7.99, 2));
 		products.add(new Product("Salami", 9.55, 3));
 		return products;
 	}
 	
-	private void getMergedOrder() {
+	public String getMergedOrder(String orderId) {
 		MergeOrderClient merge = new MergeOrderClient();
 		try {
-			String result = merge.getMergedOrder("1680778200745");
-			System.out.println(result);
+			String result = merge.getMergedOrder(orderId);
+			return result;
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		return null;
 	}
 	
-	private void getNumberOfOrders() {
+	public void getNumberOfOrders() {
 		GetNumberOfOrderClient getNumber = new GetNumberOfOrderClient();
 		try {
 			int number = getNumber.getNumberOfOrdersFromOrderList("1680778200745");
@@ -94,6 +102,16 @@ public class Client {
 			e.printStackTrace();
 		}
 		
+	}
+	
+	public void register () throws Exception {
+		this.id = RegistratorClient.register(); 
+	}
+	
+	public List<OrderList> getActiveOrders() throws Exception {
+		RequestAktivOrderClient request = new RequestAktivOrderClient();
+		List<OrderList> activOrders = new ArrayList<OrderList>(Arrays.asList(request.checkForAktivOrder()));
+		return activOrders;
 	}
 
 }
