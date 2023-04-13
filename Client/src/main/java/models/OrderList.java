1bc8b2f1662301bcbdfc6fc1bf0ecf9177f7a52b
Jennif6r <85253972+Jennif6r@users.noreply.github.com>
2023-04-13 11:21:37 +0200

    fix get aktiv orders with usernames

--- Client/src/main/java/models/OrderList.java
+++ Client/src/main/java/models/OrderList.java
@@ -7,10 +7,12 @@ public class OrderList {
 	private List<Order> orders;
 	private boolean aktiv;
 	private String orderId;
+	private String user; 
 	
-	public OrderList() {
+	public OrderList(String username) {
 		this.aktiv = true;
 		this.orderId = Long.toString(System.currentTimeMillis());
+		this.user = username;
 	}
 	
 //	public void addOrderListToAllOrders() {
